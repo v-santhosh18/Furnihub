a2e2b8380411e44e70b3717b3fb455b8dee074d3
Santhosh Velmurugan <vsanthosh9943@gmail.com>
2024-04-03 11:58:50 +0530

    backend
    backend r

---
+++ backend/furnihub/src/main/java/com/santhoshv/furnihub/FurnihubApplication.java
@@ -0,0 +1,13 @@
+package com.santhoshv.furnihub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FurnihubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FurnihubApplication.class, args);
+	}
+
+}
